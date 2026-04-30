f834560ccf055bfcddf0ffc6c9f557d574bf9530
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-04-30 11:45:58 +0530

    refactor: remove Shortcut_Dimension_1_Code from qc-receipt column configuration

--- components/forms/qc-receipt/column-config.ts
+++ components/forms/qc-receipt/column-config.ts
@@ -165,7 +165,6 @@ export const REQUIRED_DETAIL_FIELDS = [
   "Purchase_Order_No",
   "Purchase_Receipt_No",
   "Item_Tracking",
-  "Shortcut_Dimension_1_Code",
   "Shortcut_Dimension_2_Code",
 ];
 
